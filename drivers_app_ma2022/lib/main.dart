9270332ba791ecdae46380f0d62a4e3615d0a031
saci zakaria <sacideveloppeur@gmail.com>
2022-02-14 14:55:47 +0100

    MyApp

--- drivers_app_ma2022/lib/main.dart
+++ drivers_app_ma2022/lib/main.dart
@@ -2,39 +2,40 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(
+    MyApp(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'DRIVERS APP',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Welcome to Drivers App"),
+          ),
+          body: Center(
+            child: Text("body App"),
+          ),
+        ),
+      ), //Error
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DRIVERS APP',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Welcome to Drivers App"),
-        ),
-        body: Center(
-          child: Text("body App"),
-        ),
-      ),
-    );
+    return Container();
   }
 }
+
