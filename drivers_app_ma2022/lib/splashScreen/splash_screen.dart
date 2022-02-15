60971104e0092f5f330e282cffdd4860496c59b9
saci zakaria <sacideveloppeur@gmail.com>
2022-02-15 18:13:05 +0100

    MySplashScreen

---
+++ drivers_app_ma2022/lib/splashScreen/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
