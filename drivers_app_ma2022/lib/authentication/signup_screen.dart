ca5591da96461fccda1dec59d160b72b91bd73ed
saci zakaria <sacideveloppeur@gmail.com>
2022-03-18 22:25:16 +0100

    TextField()

--- drivers_app_ma2022/lib/authentication/signup_screen.dart
+++ drivers_app_ma2022/lib/authentication/signup_screen.dart
@@ -15,12 +15,16 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Image.asset("images/logo1.png"),
             ),
-            const SizedBox(height: 10,),
+            const SizedBox(
+              height: 10,
+            ),
             const Text(
               "Register as a Driver",
               style: TextStyle(
@@ -29,6 +33,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 fontWeight: FontWeight.bold,
               ),
             ),
+            TextField(),
           ],
         ),
       ),
