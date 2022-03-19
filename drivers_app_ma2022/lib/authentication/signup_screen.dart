cac1dc8b5d4899e05ad119f8f3d71f1c6eddb1c6
saci zakaria <sacideveloppeur@gmail.com>
2022-03-19 17:14:50 +0100

    Padding Column

--- drivers_app_ma2022/lib/authentication/signup_screen.dart
+++ drivers_app_ma2022/lib/authentication/signup_screen.dart
@@ -15,51 +15,54 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Scaffold(
       backgroundColor: Colors.black54,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Image.asset("images/logo1.png"),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "Register as a Driver",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            TextField(
-              controller: nameTextEditingController,
-              style: const TextStyle(
-                  color: Colors.grey
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("images/logo1.png"),
               ),
-              decoration: const InputDecoration(
-                labelText: "Name",
-                hintText: "Name",
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 10,
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Register as a Driver",
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-          ],
+              TextField(
+                controller: nameTextEditingController,
+                style: const TextStyle(
+                    color: Colors.grey
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Name",
+                  hintText: "Name",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
