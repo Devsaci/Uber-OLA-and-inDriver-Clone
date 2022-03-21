5eb8a8b13d23559ef3fd095cef9e661d146ce31a
saci zakaria <sacideveloppeur@gmail.com>
2022-03-21 22:31:46 +0100

    Padding

--- drivers_app_ma2022/lib/authentication/car_info_screen.dart
+++ drivers_app_ma2022/lib/authentication/car_info_screen.dart
@@ -17,97 +17,100 @@ class _CarInfoScreenState extends State<CarInfoScreen> {
     return Scaffold(
       backgroundColor: Colors.black87,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 24,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Image.asset("images/logo1.png"),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "Write Car Details",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 24,
               ),
-            ),
-            TextField(
-              controller: carModelTextEditingController,
-              style: const TextStyle(color: Colors.grey),
-              decoration: const InputDecoration(
-                labelText: "Car Model",
-                hintText: "Car Model",
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("images/logo1.png"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Write Car Details",
+                style: TextStyle(
+                  fontSize: 26,
                   color: Colors.grey,
-                  fontSize: 10,
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            TextField(
-              controller: carNumberTextEditingController,
-              style: const TextStyle(
-                  color: Colors.grey
-              ),
-              decoration: const InputDecoration(
-                labelText: "Car Number",
-                hintText: "Car Number",
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 10,
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
+              TextField(
+                controller: carModelTextEditingController,
+                style: const TextStyle(color: Colors.grey),
+                decoration: const InputDecoration(
+                  labelText: "Car Model",
+                  hintText: "Car Model",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
                 ),
               ),
-            ),
-            TextField(
-              controller: carColorTextEditingController,
-              style: const TextStyle(
-                  color: Colors.grey
+              TextField(
+                controller: carNumberTextEditingController,
+                style: const TextStyle(
+                    color: Colors.grey
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Car Number",
+                  hintText: "Car Number",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
               ),
-              decoration: const InputDecoration(
-                labelText: "Car Color",
-                hintText: "Car Color",
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
+              TextField(
+                controller: carColorTextEditingController,
+                style: const TextStyle(
+                    color: Colors.grey
                 ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey),
-                ),
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 10,
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 14,
+                decoration: const InputDecoration(
+                  labelText: "Car Color",
+                  hintText: "Car Color",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 10,
+                  ),
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
