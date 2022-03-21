fed3d4ba0398753f9c0a79534114bc0f081f1a99
saci zakaria <sacideveloppeur@gmail.com>
2022-03-21 21:59:40 +0100

    class CarInfoScreen

---
+++ drivers_app_ma2022/lib/authentication/car_info_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class CarInfoScreen extends StatefulWidget {
+
+  @override
+  State<CarInfoScreen> createState() => _CarInfoScreenState();
+}
+
+class _CarInfoScreenState extends State<CarInfoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
