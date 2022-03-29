1b5380709e12b32f76d07a1ca2c40cd666903e52
saci zakaria <sacideveloppeur@gmail.com>
2022-03-29 11:34:37 +0200

    Text : "Login as a Driver"

--- drivers_app_ma2022/lib/authentication/login_screen.dart
+++ drivers_app_ma2022/lib/authentication/login_screen.dart
@@ -1,9 +1,8 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen  extends StatefulWidget {
-  const LoginScreen ({Key? key}) : super(key: key);
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -12,18 +11,29 @@ class LoginScreen  extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Column(
-          children:  [
-            const SizedBox(width: 10,),
+          children: [
+            const SizedBox(
+              width: 10,
+            ),
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: Image.asset("images/logo1.png"),
             ),
+            const SizedBox(
+              width: 10,
+            ),
+            const Text(
+              "Login as a Driver",
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.grey
+              ),
+            ),
           ],
-
         ),
       ),
     );
