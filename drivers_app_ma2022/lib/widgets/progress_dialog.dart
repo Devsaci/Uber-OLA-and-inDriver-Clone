a5240aead1f6bc4a0482a2864cf24174e3797c5a
saci zakaria <sacideveloppeur@gmail.com>
2022-03-31 17:44:32 +0200

    ProgressDialog(this.message);

--- drivers_app_ma2022/lib/widgets/progress_dialog.dart
+++ drivers_app_ma2022/lib/widgets/progress_dialog.dart
@@ -1,11 +1,11 @@
-
-
 import 'package:flutter/material.dart';
 
-class ProgressDialog  extends StatelessWidget {
+class ProgressDialog extends StatelessWidget {
   // const ProgressDialog ({Key? key}) : super(key: key);
 
   String? message;
+
+  ProgressDialog(this.message);
 
   @override
   Widget build(BuildContext context) {
