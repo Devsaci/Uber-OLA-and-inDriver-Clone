09e763fc7ba3db09c3d8f0f1a7463ea31db7aa73
saci zakaria <sacideveloppeur@gmail.com>
2022-04-17 18:57:07 +0200

    Add Repo SplashScreen + Repo Global + images/logo.png

---
+++ users_app_ma2022/lib/global/global.dart
@@ -0,0 +1,6 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+final FirebaseAuth fAuth = FirebaseAuth.instance;
+User? currentFirebaseUser;
---
+++ users_app_ma2022/lib/splashScreen/splash_screen.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:users_app/mainScreens/main_screen.dart';
+
+import '../global/global.dart';
+
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer()
+  {
+    Timer(const Duration(seconds: 3), () async
+    {
+      if(await fAuth.currentUser != null)
+      {
+        currentFirebaseUser = fAuth.currentUser;
+        Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
+      }
+      else
+      {
+        //Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/logo1.png"),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Uber & inDriver Clone App",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
