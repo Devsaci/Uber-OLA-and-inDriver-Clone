e3fd711092319b7eafd13fc18730cbd04bd31de8
saci zakaria <sacideveloppeur@gmail.com>
2022-05-05 21:36:18 +0200

    myKey IOS

--- drivers_app_ma2022/ios/Runner/AppDelegate.swift
+++ drivers_app_ma2022/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyAPxmJiF4JR36J_wB2rjycTt6ABn3GJNZ8")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
