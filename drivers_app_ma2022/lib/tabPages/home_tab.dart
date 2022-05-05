6a8faff4ea2df434c812753eaad8ed905b85a2b4
saci zakaria <sacideveloppeur@gmail.com>
2022-05-05 21:38:54 +0200

    static final CameraPosition

--- drivers_app_ma2022/lib/tabPages/home_tab.dart
+++ drivers_app_ma2022/lib/tabPages/home_tab.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/cupertino.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({Key? key}) : super(key: key);
@@ -10,6 +11,13 @@ class HomeTabPage extends StatefulWidget {
 }
 
 class _HomeTabPageState extends State<HomeTabPage> {
+
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return   Center(
