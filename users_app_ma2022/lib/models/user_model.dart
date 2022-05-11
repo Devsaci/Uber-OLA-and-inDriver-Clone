fc328ba877ac7352e09529c47af474f979ae383e
saci zakaria <sacideveloppeur@gmail.com>
2022-05-11 18:49:00 +0200

    Generated Constructor UserModel

--- users_app_ma2022/lib/models/user_model.dart
+++ users_app_ma2022/lib/models/user_model.dart
@@ -1,7 +1,9 @@
 // 29. user Model Class
-class UserModel{
+class UserModel {
   String? phone;
   String? name;
   String? id;
   String? email;
+
+  UserModel(this.phone, this.name, this.id, this.email);
 }
