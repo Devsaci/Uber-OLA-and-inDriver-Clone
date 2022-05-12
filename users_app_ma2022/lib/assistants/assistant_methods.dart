c686ebc86b73815059d9611b75d1d8c52b28f08c
saci zakaria <sacideveloppeur@gmail.com>
2022-05-12 22:37:10 +0200

    UserModel.fromSnapshot(snap.snapshot);

--- users_app_ma2022/lib/assistants/assistant_methods.dart
+++ users_app_ma2022/lib/assistants/assistant_methods.dart
@@ -1,6 +1,7 @@
 //30. Read and Display Current Online user info from Database
 import 'package:firebase_database/firebase_database.dart';
 import 'package:users_app/global/global.dart';
+import 'package:users_app/models/user_model.dart';
 
 class AssistantMethods {
   static void readCurrentOnlineUserInfo() {
@@ -11,7 +12,9 @@ class AssistantMethods {
         .child(currentFirebaseUser!.uid);
 
     userRef.once().then((snap) {
-      if(snap.snapshot.value != null){}
+      if (snap.snapshot.value != null) {
+        UserModel.fromSnapshot(snap.snapshot);
+      }
     });
   }
 }
