24a05bbe915531811580638611071a4416f1400f
saci zakaria <sacideveloppeur@gmail.com>
2022-05-12 22:19:44 +0200

    FirebaseAuth.instance.currentUser;

--- users_app_ma2022/lib/assistants/assistant_methods.dart
+++ users_app_ma2022/lib/assistants/assistant_methods.dart
@@ -1,5 +1,10 @@
 //30. Read and Display Current Online user info from Database
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 class AssistantMethods{
-  static void readCurrentOnlineUserInfo(){}
+  static void readCurrentOnlineUserInfo(){
+   FirebaseAuth.instance.currentUser;
+  }
 }
