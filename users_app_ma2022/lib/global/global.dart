83205d26075a710e76ae90179f753333358f7103
saci zakaria <sacideveloppeur@gmail.com>
2022-05-12 22:39:25 +0200

    UserModel? userModelCurrentInfo;

--- users_app_ma2022/lib/global/global.dart
+++ users_app_ma2022/lib/global/global.dart
@@ -1,6 +1,9 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../models/user_model.dart';
+
 
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 User? currentFirebaseUser;
+UserModel? userModelCurrentInfo;
