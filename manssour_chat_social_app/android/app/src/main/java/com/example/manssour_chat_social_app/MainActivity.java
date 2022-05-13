28c2b60c285b23bbe1524bdf2fb2b7fdf28627e5
saci zakaria <sacideveloppeur@gmail.com>
2022-05-13 17:14:04 +0200

    Section 9 : Firebase and Clouding / 116. Firebase Authentication

--- manssour_chat_social_app/android/app/src/main/java/com/example/manssour_chat_social_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.manssour_chat_social_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ users_app_ma2022/lib/widgets/my_drawer.dart
@@ -0,0 +1,1 @@
+//31. Navigation Drawer - Drawer Header
