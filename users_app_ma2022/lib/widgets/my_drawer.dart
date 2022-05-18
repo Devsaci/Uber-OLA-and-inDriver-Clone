c146be8d4d7dd79fcac4b8a6518a63079ba49cb5
saci zakaria <sacideveloppeur@gmail.com>
2022-05-18 10:45:37 +0200

    class MyDrawer extends StatefulWidget

--- users_app_ma2022/lib/widgets/my_drawer.dart
+++ users_app_ma2022/lib/widgets/my_drawer.dart
@@ -2,9 +2,14 @@
 
 
 import 'package:flutter/material.dart';
-class MyDrawer extends StatelessWidget {
+class MyDrawer extends StatefulWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
     return Container();
