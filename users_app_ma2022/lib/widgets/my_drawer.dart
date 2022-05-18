2c913cfa89ca100c2ca676b5bb64b138b6d629ec
saci zakaria <sacideveloppeur@gmail.com>
2022-05-18 10:37:48 +0200

    class MyDrawer extends StatefulWidget

--- users_app_ma2022/lib/widgets/my_drawer.dart
+++ users_app_ma2022/lib/widgets/my_drawer.dart
@@ -1,1 +1,18 @@
 // 31. Navigation Drawer - Drawer Header
+
+
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatefulWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
