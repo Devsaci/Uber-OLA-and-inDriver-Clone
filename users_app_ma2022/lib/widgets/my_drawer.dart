763c127d6bed28381753eb8f6e88415ee06e151c
saci zakaria <sacideveloppeur@gmail.com>
2022-05-25 18:36:07 +0200

    onTap: (){},

--- users_app_ma2022/lib/widgets/my_drawer.dart
+++ users_app_ma2022/lib/widgets/my_drawer.dart
@@ -60,7 +60,9 @@ class _MyDrawerState extends State<MyDrawer> {
           const SizedBox(height: 12.0),
           //drawer body
           //33. Navigation Drawer - Drawer Body
-          GestureDetector(),
+          GestureDetector(
+            onTap: (){},
+          ),
         ],
       ),
     );
