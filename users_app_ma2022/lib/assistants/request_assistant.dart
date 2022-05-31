25cb5c60e541306c56bb183566ba9268d31a89e0
saci zakaria <sacideveloppeur@gmail.com>
2022-05-31 23:05:39 +0200

    await http.get(Uri.parse(url));

--- users_app_ma2022/lib/assistants/request_assistant.dart
+++ users_app_ma2022/lib/assistants/request_assistant.dart
@@ -1,3 +1,7 @@
+import 'package:http/http.dart' as http;
+
 class RequestAssistant {
-  static Future<dynamic> receiveRequest(String url) async {}
+  static Future<dynamic> receiveRequest(String url) async {
+    await http.get(Uri.parse(url));
+  }
 }
