9ddbbe4815c4245452f5f0a04854618c589ddff6
saci zakaria <sacideveloppeur@gmail.com>
2022-05-31 23:16:55 +0200

    try / catch

--- users_app_ma2022/lib/assistants/request_assistant.dart
+++ users_app_ma2022/lib/assistants/request_assistant.dart
@@ -5,6 +5,13 @@ import 'package:http/http.dart' as http;
 class RequestAssistant {
   static Future<dynamic> receiveRequest(String url) async {
     http.Response httpResponse = await http.get(Uri.parse(url));
+
+    try {
+
+    } catch (exp) {
+      return "Error Occurred, Failed. No Response.";
+    }
+
     if (httpResponse.statusCode == 200) {
       String responseData = httpResponse.body; //json
       var decodeResponseData = jsonDecode(responseData);
