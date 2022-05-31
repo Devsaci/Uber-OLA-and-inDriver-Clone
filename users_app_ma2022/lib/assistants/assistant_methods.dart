aa2ebf28b909e5a2935576da4bffb60852d01fc6
saci zakaria <sacideveloppeur@gmail.com>
2022-05-31 23:25:35 +0200

    static Future<String?> searchAddressForGeographicCoOrdinates(       Position position) async {}

--- users_app_ma2022/lib/assistants/assistant_methods.dart
+++ users_app_ma2022/lib/assistants/assistant_methods.dart
@@ -1,9 +1,13 @@
 //30. Read and Display Current Online user info from Database
 import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:users_app/global/global.dart';
 import 'package:users_app/models/user_model.dart';
 
 class AssistantMethods {
+  static Future<String?> searchAddressForGeographicCoOrdinates(
+      Position position) async {}
+
   static void readCurrentOnlineUserInfo() {
     currentFirebaseUser = fAuth.currentUser;
     DatabaseReference userRef = FirebaseDatabase.instance
@@ -13,7 +17,7 @@ class AssistantMethods {
 
     userRef.once().then((snap) {
       if (snap.snapshot.value != null) {
-        userModelCurrentInfo =UserModel.fromSnapshot(snap.snapshot);
+        userModelCurrentInfo = UserModel.fromSnapshot(snap.snapshot);
         // For Testing methode AssistantMethods
         print("******** name   =  " + userModelCurrentInfo!.name.toString());
         print("******** email  =  " + userModelCurrentInfo!.email.toString());
