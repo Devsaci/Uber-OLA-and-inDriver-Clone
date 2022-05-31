4ea9a5338f87069713eae92771e01022dc3b1986
saci zakaria <sacideveloppeur@gmail.com>
2022-05-31 23:07:44 +0200

    if(httpResponse.statusCode == 200){}

--- users_app_ma2022/lib/assistants/request_assistant.dart
+++ users_app_ma2022/lib/assistants/request_assistant.dart
@@ -3,5 +3,6 @@ import 'package:http/http.dart' as http;
 class RequestAssistant {
   static Future<dynamic> receiveRequest(String url) async {
     http.Response httpResponse = await http.get(Uri.parse(url));
+    if(httpResponse.statusCode == 200){} //successful
   }
 }
