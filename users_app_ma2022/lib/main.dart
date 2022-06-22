937aaa1cd959ac7e21f03d7f918d299f90f0a1dd
saci zakaria <sacideveloppeur@gmail.com>
2022-06-23 00:49:12 +0200

    ChangeNotifierProvider

--- users_app_ma2022/lib/main.dart
+++ users_app_ma2022/lib/main.dart
@@ -1,23 +1,29 @@
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:users_app/splashScreen/splash_screen.dart';
+import 'package:users_app/infoHandler/app_info.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(
     MyApp(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'DRIVERS APP',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Welcome to Drivers App"),
+      child: ChangeNotifierProvider(
+        create: (context) => AppInfo(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'DRIVERS APP',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
           ),
-          body: const MySplashScreen(),
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Welcome to Drivers App"),
+            ),
+            body: const MySplashScreen(),
+          ),
         ),
       ),
     ),
