165c25c9eddb70f7760a2f6d5788eb91572096c4
saci zakaria <sacideveloppeur@gmail.com>
2022-06-24 11:08:03 +0200

    class SearchPlacesScreen

---
+++ users_app_ma2022/lib/mainScreens/search_places_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class SearchPlacesScreen extends StatefulWidget {
+  const SearchPlacesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPlacesScreen> createState() => _SearchPlacesScreenState();
+}
+
+class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
