1ee438fbf207d932972a016a4f198ee106149c21
saci zakaria <sacideveloppeur@gmail.com>
2022-06-24 11:29:39 +0200

    boxShadow: BoxShadow

--- users_app_ma2022/lib/mainScreens/search_places_screen.dart
+++ users_app_ma2022/lib/mainScreens/search_places_screen.dart
@@ -15,7 +15,24 @@ class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
       backgroundColor: Colors.black87,
       body: Column(
         children: [
-          Container(),
+          //search place ui
+          Container(
+            height: 160,
+            decoration: const BoxDecoration(
+              color: Colors.black54,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white54,
+                  blurRadius: 8,
+                  spreadRadius: 0.5,
+                  offset: Offset(
+                    0.7,
+                    0.7,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
