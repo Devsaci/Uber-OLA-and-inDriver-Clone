45f330175a7cd7a6bf3673bc9eb5708386762317
saci zakaria <sacideveloppeur@gmail.com>
2022-06-27 21:20:56 +0200

    contentPadding: EdgeInsets.only

--- users_app_ma2022/lib/mainScreens/search_places_screen.dart
+++ users_app_ma2022/lib/mainScreens/search_places_screen.dart
@@ -71,6 +71,11 @@ class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
                         fillColor: Colors.white54,
                         filled: true,
                         border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(
+                          left: 11.0,
+                          top: 8.0,
+                          bottom: 8.0,
+                        ),
                       ),
                     ),
                   ],
