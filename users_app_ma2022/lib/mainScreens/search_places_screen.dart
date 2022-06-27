62240e782972fd57778164ed5cc1cb6eaa46dcf3
saci zakaria <sacideveloppeur@gmail.com>
2022-06-27 21:45:25 +0200

    Padding

--- users_app_ma2022/lib/mainScreens/search_places_screen.dart
+++ users_app_ma2022/lib/mainScreens/search_places_screen.dart
@@ -32,61 +32,64 @@ class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
                 ),
               ],
             ),
-            child: Column(
-              children: [
-                const SizedBox(height: 25.0),
-                Stack(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Icon(
-                        Icons.arrow_back,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    const Center(
-                      child: Text(
-                        " Search & Set DropOff Lo²cation",
-                        style: TextStyle(
-                          fontSize: 18.0,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 25.0),
+                  Stack(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.arrow_back,
                           color: Colors.grey,
-                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    const Icon(
-                      Icons.adjust_sharp,
-                      color: Colors.grey,
-                    ),
-                    const SizedBox(width: 18.0),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          onChanged: (valueTyped) {},
-                          decoration: const InputDecoration(
-                            hintText: "search here...",
-                            fillColor: Colors.white54,
-                            filled: true,
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.only(
-                              left: 11.0,
-                              top: 8.0,
-                              bottom: 8.0,
+                      const Center(
+                        child: Text(
+                          " Search & Set DropOff Lo²cation",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.adjust_sharp,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(width: 18.0),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            onChanged: (valueTyped) {},
+                            decoration: const InputDecoration(
+                              hintText: "search here...",
+                              fillColor: Colors.white54,
+                              filled: true,
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(
+                                left: 11.0,
+                                top: 8.0,
+                                bottom: 8.0,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
