6299746a5e48d68cf851af19e96d829012234a90
saci zakaria <sacideveloppeur@gmail.com>
2022-06-27 19:43:25 +0200

    hintText:"search here...",

--- users_app_ma2022/lib/mainScreens/search_places_screen.dart
+++ users_app_ma2022/lib/mainScreens/search_places_screen.dart
@@ -67,7 +67,7 @@ class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
                     SizedBox(width: 18.0),
                     TextField(
                       decoration: InputDecoration(
-
+                          hintText:"search here...",
                       ),
                     ),
                   ],
