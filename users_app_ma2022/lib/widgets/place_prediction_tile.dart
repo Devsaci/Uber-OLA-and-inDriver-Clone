29ef18ff184d96aaee5c93a251a09d779d706363
saci zakaria <sacideveloppeur@gmail.com>
2022-09-09 22:23:43 +0200

    class PlacePredictionTileDesign

---
+++ users_app_ma2022/lib/widgets/place_prediction_tile.dart
@@ -0,0 +1,17 @@
+//49. Convert, Add json Response Data to List - Display Places Predictions on ListView
+
+import 'package:flutter/material.dart';
+
+class PlacePredictionTileDesign extends StatefulWidget {
+  const PlacePredictionTileDesign({Key? key}) : super(key: key);
+
+  @override
+  State<PlacePredictionTileDesign> createState() => _PlacePredictionTileDesignState();
+}
+
+class _PlacePredictionTileDesignState extends State<PlacePredictionTileDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
