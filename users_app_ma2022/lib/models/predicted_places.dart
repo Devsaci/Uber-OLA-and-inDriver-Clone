5b20450e0d40870933da828fc98a27a5831950aa
saci zakaria <sacideveloppeur@gmail.com>
2022-09-09 16:58:37 +0200

    PredictedPlaces(this.place_id, this.main_text, this.secondary_text);

--- users_app_ma2022/lib/models/predicted_places.dart
+++ users_app_ma2022/lib/models/predicted_places.dart
@@ -1,2 +1,8 @@
 //49. Convert, Add json Response Data to List - Display Places Predictions on ListView
-class PredictedPlaces{}
+class PredictedPlaces {
+  String? place_id;
+  String? main_text;
+  String? secondary_text;
+
+  PredictedPlaces(this.place_id, this.main_text, this.secondary_text);
+}
