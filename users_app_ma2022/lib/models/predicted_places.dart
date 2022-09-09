1f92fa5f1090db88572ade48ebaf12c1045f097b
saci zakaria <sacideveloppeur@gmail.com>
2022-09-09 17:08:29 +0200

    PredictedPlaces.fromJson(Map<String, dynamic> jsonData)

--- users_app_ma2022/lib/models/predicted_places.dart
+++ users_app_ma2022/lib/models/predicted_places.dart
@@ -5,4 +5,10 @@ class PredictedPlaces {
   String? secondary_text;
 
   PredictedPlaces(this.place_id, this.main_text, this.secondary_text);
+
+  PredictedPlaces.fromJson(Map<String, dynamic> jsonData) {
+    place_id = jsonData["place_id"];
+    main_text = jsonData["structured_formatting"]["main_text"];
+    secondary_text = jsonData["structured_formatting"]["secondary_text"];
+  }
 }
