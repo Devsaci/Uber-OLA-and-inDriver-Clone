2d58326dc3ec6e85fdeb7dabb05226a2f3fc0838
saci zakaria <sacideveloppeur@gmail.com>
2022-09-11 18:37:20 +0200

    (placesPredictedList.length > 0) ListView.separated

--- users_app_ma2022/lib/mainScreens/search_places_screen.dart
+++ users_app_ma2022/lib/mainScreens/search_places_screen.dart
@@ -14,12 +14,13 @@ class SearchPlacesScreen extends StatefulWidget {
 class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
   // 48. Enable Places API [Auto Complete Place Search]
   List<PredictedPlaces> placesPredictedList = [];
+
   void findPlaceAutoCompleteSearch(String inputText) async {
     if (inputText.length > 1) {
       String urlAutoCompleteSearch =
           "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$inputText&key=$mapKey&components=country:FR";
       var responseAutoCompleteSearch =
-          await RequestAssistant.receiveRequest(urlAutoCompleteSearch);
+      await RequestAssistant.receiveRequest(urlAutoCompleteSearch);
       if (responseAutoCompleteSearch ==
           "Error Occurred, Failed. No Response.") {
         return;
@@ -42,90 +43,92 @@ class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
       backgroundColor: Colors.black87,
       body: Column(
         children: [
-          //search place ui
-          Container(
-            height: 160,
-            decoration: const BoxDecoration(
-              color: Colors.black54,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.white54,
-                  blurRadius: 8,
-                  spreadRadius: 0.5,
-                  offset: Offset(
-                    0.7,
-                    0.7,
-                  ),
-                ),
-              ],
+        //search place ui
+        Container(
+        height: 160,
+        decoration: const BoxDecoration(
+          color: Colors.black54,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white54,
+              blurRadius: 8,
+              spreadRadius: 0.5,
+              offset: Offset(
+                0.7,
+                0.7,
+              ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 30.0),
+              Stack(
                 children: [
-                  const SizedBox(height: 30.0),
-                  Stack(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.amber,
-                        ),
-                      ),
-                      const Center(
-                        child: Text(
-                          "Search & Set DropOff Location",
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.amber,
+                    ),
                   ),
-                  const SizedBox(height: 10.0),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.adjust_sharp,
-                        color: Colors.amber,
-                        size: 30.0,
+                  const Center(
+                    child: Text(
+                      "Search & Set DropOff Location",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
                       ),
-                      const SizedBox(width: 18.0),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextField(
-                            onChanged: (valueTyped) {
-                              findPlaceAutoCompleteSearch(valueTyped);
-                            },
-                            decoration: const InputDecoration(
-                              hintText: "search here...",
-                              fillColor: Colors.white54,
-                              filled: true,
-                              border: InputBorder.none,
-                              contentPadding: EdgeInsets.only(
-                                left: 11.0,
-                                top: 8.0,
-                                bottom: 8.0,
-                              ),
-                            ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.adjust_sharp,
+                    color: Colors.amber,
+                    size: 30.0,
+                  ),
+                  const SizedBox(width: 18.0),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        onChanged: (valueTyped) {
+                          findPlaceAutoCompleteSearch(valueTyped);
+                        },
+                        decoration: const InputDecoration(
+                          hintText: "search here...",
+                          fillColor: Colors.white54,
+                          filled: true,
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.only(
+                            left: 11.0,
+                            top: 8.0,
+                            bottom: 8.0,
                           ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ],
               ),
-            ),
+            ],
           ),
-          //display place predictions result
-        ],
+        ),
       ),
-    );
+      //display place predictions result
+      (placesPredictedList.length > 0)
+          ? Expanded(child: Text('ListView.separated'))
+          : Container(),
+      ],
+    ),);
   }
 }
