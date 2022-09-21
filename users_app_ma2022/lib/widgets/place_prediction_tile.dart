3a208e1fc6a5baaf3a95aa1bef690d8edf9f19ca
saci zakaria <sacideveloppeur@gmail.com>
2022-09-21 18:27:04 +0200

    responseApi["result"]["geometry"]["location"]["lat"];

--- users_app_ma2022/lib/widgets/place_prediction_tile.dart
+++ users_app_ma2022/lib/widgets/place_prediction_tile.dart
@@ -29,6 +29,7 @@ class PlacePredictionTileDesign extends StatelessWidget {
     if (responseApi["status"] == "OK") {
       Directions direction = Directions();
       direction.locationName = responseApi["result"]["name"];
+      responseApi["result"]["geometry"]["location"]["lat"];
     }
   }
 
