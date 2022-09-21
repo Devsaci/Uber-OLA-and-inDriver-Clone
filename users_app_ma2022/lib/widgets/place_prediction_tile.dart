0e493cc5121e08880efd74c6fc5d4ff15f7be5e8
saci zakaria <sacideveloppeur@gmail.com>
2022-09-21 08:44:01 +0200

    padding: const EdgeInsets.all(4.0),

--- users_app_ma2022/lib/widgets/place_prediction_tile.dart
+++ users_app_ma2022/lib/widgets/place_prediction_tile.dart
@@ -16,42 +16,45 @@ class PlacePredictionTileDesign extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         primary: Colors.white24,
       ),
-      child: Row(
-        children:  [
-          const Icon(
-            Icons.add_location,
-            color: Colors.grey,
-          ),
-          const SizedBox(
-            width: 14.0,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 8.0,),
-                Text(
-                  predictedPlaces!.main_text!,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 16.0,
-                    color: Colors.white54,
-                  ),
-                ),
-                const SizedBox(height: 2.0,),
-                Text(
-                  predictedPlaces!.secondary_text!,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 12.0,
-                    color: Colors.white54,
-                  ),
-                ),
-                const SizedBox(height: 8.0,),
-              ],
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Row(
+          children:  [
+            const Icon(
+              Icons.add_location,
+              color: Colors.grey,
             ),
-          )
-        ],
+            const SizedBox(
+              width: 14.0,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 8.0,),
+                  Text(
+                    predictedPlaces!.main_text!,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white54,
+                    ),
+                  ),
+                  const SizedBox(height: 2.0,),
+                  Text(
+                    predictedPlaces!.secondary_text!,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.white54,
+                    ),
+                  ),
+                  const SizedBox(height: 8.0,),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
