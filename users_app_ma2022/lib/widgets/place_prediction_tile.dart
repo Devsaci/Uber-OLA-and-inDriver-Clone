d49c785019314657cab27e5bae6e2aedb46af8fa
saci zakaria <sacideveloppeur@gmail.com>
2022-09-21 16:36:30 +0200

    $mapKey

--- users_app_ma2022/lib/widgets/place_prediction_tile.dart
+++ users_app_ma2022/lib/widgets/place_prediction_tile.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:users_app/widgets/progress_dialog.dart';
 
+import '../global/map_key.dart';
 import '../models/predicted_places.dart';
 
 class PlacePredictionTileDesign extends StatelessWidget {
