1b131b2313dfdba00d5c3bd9ba604edfffc694f7
saci zakaria <sacideveloppeur@gmail.com>
2022-09-23 15:49:04 +0200

    DirectionDetailsInfo({})

--- users_app_ma2022/lib/models/direction_details_info.dart
+++ users_app_ma2022/lib/models/direction_details_info.dart
@@ -1,3 +1,17 @@
 // 54. direction details api - get direction details from origin to destination
 
-class DirectionDetailsInfo {}
+class DirectionDetailsInfo {
+  int? distance_value;
+  int? duration_value;
+  String? e_points;
+  String? distance_text;
+  String? duration_text;
+
+  DirectionDetailsInfo({
+    this.distance_value,
+    this.duration_value,
+    this.e_points,
+    this.distance_text,
+    this.duration_text,
+  });
+}
