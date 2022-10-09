719c385833201ccdc9bfa7ff7460cb81e37963fb
saci zakaria <sacideveloppeur@gmail.com>
2022-10-09 15:02:23 +0200

    Assistance Repo / 22 error

---
+++ drivers_app_ma2022/lib/assistants/assistant_methods.dart
@@ -0,0 +1,79 @@
+import 'package:drivers_app/assistants/request_assistant.dart';
+import 'package:drivers_app/global/global.dart';
+import 'package:drivers_app/global/map_key.dart';
+import 'package:drivers_app/infoHandler/app_info.dart';
+import 'package:drivers_app/models/direction_details_info.dart';
+import 'package:drivers_app/models/directions.dart';
+import 'package:drivers_app/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+
+
+class AssistantMethods
+{
+  static Future<String> searchAddressForGeographicCoOrdinates(Position position, context) async
+  {
+    String apiUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
+    String humanReadableAddress="";
+
+    var requestResponse = await RequestAssistant.receiveRequest(apiUrl);
+
+    if(requestResponse != "Error Occurred, Failed. No Response.")
+    {
+      humanReadableAddress = requestResponse["results"][0]["formatted_address"];
+
+      Directions userPickUpAddress = Directions();
+      userPickUpAddress.locationLatitude = position.latitude;
+      userPickUpAddress.locationLongitude = position.longitude;
+      userPickUpAddress.locationName = humanReadableAddress;
+
+      Provider.of<AppInfo>(context, listen: false).updatePickUpLocationAddress(userPickUpAddress);
+    }
+
+    return humanReadableAddress;
+  }
+
+  static void readCurrentOnlineUserInfo() async
+  {
+    currentFirebaseUser = fAuth.currentUser;
+
+    DatabaseReference userRef = FirebaseDatabase.instance
+        .ref()
+        .child("users")
+        .child(currentFirebaseUser!.uid);
+
+    userRef.once().then((snap)
+    {
+      if(snap.snapshot.value != null)
+      {
+        userModelCurrentInfo = UserModel.fromSnapshot(snap.snapshot);
+      }
+    });
+  }
+
+  static Future<DirectionDetailsInfo?> obtainOriginToDestinationDirectionDetails(LatLng origionPosition, LatLng destinationPosition) async
+  {
+    String urlOriginToDestinationDirectionDetails = "https://maps.googleapis.com/maps/api/directions/json?origin=${origionPosition.latitude},${origionPosition.longitude}&destination=${destinationPosition.latitude},${destinationPosition.longitude}&key=$mapKey";
+
+    var responseDirectionApi = await RequestAssistant.receiveRequest(urlOriginToDestinationDirectionDetails);
+
+    if(responseDirectionApi == "Error Occurred, Failed. No Response.")
+    {
+      return null;
+    }
+
+    DirectionDetailsInfo directionDetailsInfo = DirectionDetailsInfo();
+    directionDetailsInfo.e_points = responseDirectionApi["routes"][0]["overview_polyline"]["points"];
+
+    directionDetailsInfo.distance_text = responseDirectionApi["routes"][0]["legs"][0]["distance"]["text"];
+    directionDetailsInfo.distance_value = responseDirectionApi["routes"][0]["legs"][0]["distance"]["value"];
+
+    directionDetailsInfo.duration_text = responseDirectionApi["routes"][0]["legs"][0]["duration"]["text"];
+    directionDetailsInfo.duration_value = responseDirectionApi["routes"][0]["legs"][0]["duration"]["value"];
+
+    return directionDetailsInfo;
+  }
+}
