a5461feadeb8e83e596862b8d6a9c0105a1cd99f
saci zakaria <sacideveloppeur@gmail.com>
2022-10-17 15:55:19 +0200

    class ActiveNearbyAvailableDrivers

--- users_app_ma2022/lib/models/active_nearby_available_drivers.dart
+++ users_app_ma2022/lib/models/active_nearby_available_drivers.dart
@@ -1,1 +1,13 @@
 // 64. QueryAtLocation [Handle GeoFire CallBacks, onKeyEntered onKeyExited onKeyMoved]
+class ActiveNearbyAvailableDrivers
+{
+  String? driverId;
+  double? locationLatitude;
+  double? locationLongitude;
+
+  ActiveNearbyAvailableDrivers({
+    this.driverId,
+    this.locationLatitude,
+    this.locationLongitude,
+  });
+}
