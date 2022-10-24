d03e51a6b38dcf6c6cbf49650992b6859a040982
saci zakaria <sacideveloppeur@gmail.com>
2022-10-24 11:05:34 +0200

    SystemNavigator.pop();

--- users_app_ma2022/lib/mainScreens/select_nearest_active_driver_screen.dart
+++ users_app_ma2022/lib/mainScreens/select_nearest_active_driver_screen.dart
@@ -1,6 +1,7 @@
 // 69. display online nearest drivers and their information
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SelectNearestActiveDriversScreen extends StatefulWidget {
   const SelectNearestActiveDriversScreen({Key? key}) : super(key: key);
@@ -14,8 +15,23 @@ class _SelectNearestActiveDriversScreenState
     extends State<SelectNearestActiveDriversScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          "Nearest Online Drivers",
+          style: TextStyle(fontSize: 18),
+        ),
+        leading: IconButton(
+            onPressed: () {
+              //delete/remove the ride request from database
+              SystemNavigator.pop();
+            },
+            icon: const Icon(
+                Icons.close, color: Colors.white
+            ),
+        ),
+      ),
     );
   }
 }
